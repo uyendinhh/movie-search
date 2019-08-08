c98ae6f401ae89250e8c127b7eec76bfa7fcc732
Uyen Dinh <udinh3@gatech.edu>
2019-08-08 11:14:32 -0400

    finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,44 +1,87 @@
 import { Component } from '@angular/core';
 import { ApiService } from './api.service'
 import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  movieTitle : string = '';
-  result : string;
+  movieTitle: string = '';
+  result: string;
   results = [];
+  selectedMovies = [];
   selectedMovie = '';
   queryField: FormControl = new FormControl();
+  has5Movies = false;
+  isDuplicad = false;
+  movieInput = '';
+  myControl = new FormControl();
+  options: string[] = [];
+  filteredOptions: Observable<string[]>;
 
-  constructor(private apiService: ApiService) {
-    if (this.queryField.valueChanges) {
-      this.queryField.valueChanges
-        .subscribe(queryField => this.apiService.getMovie(queryField)
-          .subscribe(response => {
-            console.log(this.results);
-            this.results = [];
-            if (response && response.Title.length > 0) {
-              this.results.push(response);
-            } else {
-              this.results = null;
-              queryField.reset();
-            }
-          }
-          ));
-    }
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit() {
+    
+    this.myControl.valueChanges
+      .subscribe(movieTitle => {
+        if (movieTitle != '') {
+          this.searchMovies(movieTitle);
+        } else {
+          this.results = [];
+        }
+      });
   }
 
 
   onSelected(selectedMovie: string) {
+    if (this.selectedMovies.length <= 4) {
+      if (this.selectedMovies.indexOf(selectedMovie) >= 0) {
+        this.isDuplicad = true;
+      } else {
+        this.isDuplicad = false;
+        this.selectedMovies.push(selectedMovie);
+      }
+    } else {
+      this.has5Movies = true;
+      this.isDuplicad = false;
+    }
     this.selectedMovie = selectedMovie;
+
   }
 
 
-  clickSearch(movieTitle) {
-    this.apiService.getMovie(movieTitle).subscribe(res => this.result = res.toString());
-    console.log(this.result);
+  searchMovies(movieTitle) {
+    this.apiService.getMovie(movieTitle)
+      .subscribe(response => {
+        if (response.Response != 'False' && response.Title.length > 0) {
+          if (this.results.indexOf(response.Title) < 0) {
+            this.results.push(response.Title);
+          }
+        } else {
+          this.results = [];
+        }
+      })
   }
+
+  deleteMovie(movie) {
+    this.isDuplicad = false;
+    var index = this.selectedMovies.indexOf(movie);
+    if (index > -1) {
+      this.selectedMovies.splice(index, 1);
+      this.has5Movies = false;
+    }
+    if (this.selectedMovies.length <= 4) {
+      this.has5Movies = false;
+    }
+  }
+  
+  resetInput() {
+    this.has5Movies = false;
+    this.isDuplicad = false;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-// import {MatInputModule} from '@angular/material';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { ApiService} from './api.service';
+import { ApiService } from './api.service';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatInputModule } from '@angular/material/input';
- 
-
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { IconComponent } from './icon/icon.component';
+import { MatGridListModule } from '@angular/material/grid-list';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IconComponent
   ],
   imports: [
     BrowserModule,
@@ -22,6 +26,14 @@ import { MatInputModule } from '@angular/material/input';
     FormsModule,
     HttpClientModule,
     ReactiveFormsModule,
+    MatFormFieldModule,
+    MatAutocompleteModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatIconModule,
+    MatGridListModule,
 
   ],
   providers: [ApiService],
